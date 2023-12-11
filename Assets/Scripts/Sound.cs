372ac9e9cc119ba551f50ae18bedc78e0734d733
Ruslan155 <coperonplay3@gmail.com>
2023-12-11 17:49:26 +0300

    add plugins

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Plugins.Audio.Core;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    public bool loop;
+
+    [HideInInspector]
+    public SourceAudio source;
+}
